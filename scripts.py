8be0d7e5d1146784458233689c6fd88c937bd7d4
lostblackknight <chensixiang12345@gmail.com>
2021-04-14 20:17:50 +0800

    add scripts.py

---
+++ scripts.py
@@ -0,0 +1,15 @@
+from wordmarker.contexts import WordMarkerContext
+from wordmarker.templates import CsvTemplate, PdbcTemplate
+
+
+def build_data_base():
+    """
+    构建数据库环境脚本
+    """
+    WordMarkerContext("E://PycharmProjects//wordmarker//config.yaml")
+    csv_template = CsvTemplate()
+    data_dict = csv_template.csv_to_df()
+    pdbc_template = PdbcTemplate()
+    pdbc_template.update_table(data_dict['城市数据_加盐.csv'], "t_city_data")
+    pdbc_template.update_table(data_dict['市场数据_加盐.csv'], "t_market_data")
+    pdbc_template.update_table(data_dict['景气指数_加盐.csv'], "t_prosperity_index")
