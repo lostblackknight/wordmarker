bdd408d0158d1fdb2167cd2c719caf0639abe278
lostblackknight <chensixiang12345@gmail.com>
2021-04-16 18:23:13 +0800

    格式化doc文件，将doc转化为docx文件

--- wordmarker/data/formatter.py
+++ wordmarker/data/formatter.py
@@ -1,4 +1,10 @@
+import logging
+import os
 from abc import ABCMeta, abstractmethod
+from win32com.client import DispatchEx
+from win32com.client.gencache import EnsureDispatch
+from wordmarker.contexts import SystemContext
+from wordmarker.utils import log
 
 
 class Formatter(metaclass=ABCMeta):
@@ -11,9 +17,37 @@ class Formatter(metaclass=ABCMeta):
         pass
 
 
-class CsvFormatter(Formatter):
-    def _format(self, data):
-        pass
+class DocxFormatter(Formatter, SystemContext):
+    """
+    将doc文件转换为docx文件
+    """
+
+    @log
+    def _format(self, doc_path_list: list, docx_path: str):
+        EnsureDispatch('Word.Application')
+        msword = DispatchEx("Word.Application")
+        msword.Visible = False  # 是否可见
+        msword.DisplayAlerts = 0
+        for doc_path in doc_path_list:
+            if doc_path.endswith(".doc") and not doc_path.startswith('~$'):
+                # 打开doc文件
+                doc = None
+                rename = None
+                try:
+                    doc = msword.Documents.Open(FileName=doc_path)
+                    # 重命名
+                    rename = os.path.split(doc_path)[1]
+                    rename = os.path.splitext(rename)
+                    final_file_name = docx_path + self.path_separator + rename[0] + '.docx'
+                    # 另存为docx文件
+                    doc.SaveAs(FileName=final_file_name, FileFormat=12)
+                finally:
+                    if doc:
+                        doc.Close()
+                        self._logger: logging.Logger
+                        self._logger.info(
+                            "{src}文件，成功转换为 {tgt}文件".format(src=os.path.split(doc_path)[1], tgt=rename[0] + '.docx'))
+        msword.Quit()
 
 
 class SqlFormatter(Formatter):
