47ad3c962b31ff1a03ff6da564eaa36b9a3f8e1d
lostblackknight <chensixiang12345@gmail.com>
2021-04-30 22:37:54 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='wordmarker',
-    version='0.0.3',
+    version='0.0.4',
     author='chensixiang',
     author_email='chensixiang1234@gmail.com',
     license_file='LICENSE',
