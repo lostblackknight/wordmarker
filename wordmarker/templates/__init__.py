5d1e9a56f6f1885540a935e017d73c26c5f3e155
lostblackknight <chensixiang12345@gmail.com>
2021-06-19 15:30:56 +0800

    更新版本

--- wordmarker/templates/__init__.py
+++ wordmarker/templates/__init__.py
@@ -29,4 +29,4 @@ from wordmarker.templates.pdbc_template import PdbcHelper
 from wordmarker.templates.csv_template import CsvTemplate
 from wordmarker.templates.csv_template import CsvHelper
 from wordmarker.templates.word_template import WordTemplate
-from wordmarker.templates.word_template import ImgHelper, DocxHelper
+from wordmarker.templates.word_template import ImgHelper, DocxHelper, AbstractConverter, TextHelper
